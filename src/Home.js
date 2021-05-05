cb34e64150b69c17c54b0c1bdb77b58e6537365e
cat-coder <cat-coder>
2021-05-05 08:53:37 -0500

    lesson8

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 const Home = () => {
+
+    const [name, setName] = useState("sdfsdf");
+    const [age, setAge] = useState(25)
+    const handleClick = () => {
+        setName("Behnam")
+        setAge(444)
+    }
+
+    
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name}is { age } old</p>
+            <button onClick={handleClick}>Click me</button>
+            
         </div>
      );
 }
