2c8b4a3ff8d76c70fbaae340096ac4d247f82be9
cat-coder <cat-coder>
2021-05-06 21:42:50 -0500

    df

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,12 +1,15 @@
 const Bloglist = (props) => {
 
     const blogs = props.blogsyasha
+    const handleDelete = props.handleDelete
+
     return ( 
         <div className="blog-list">
              {blogs.map((blog) => (
                <div className="blog-preview" key={blogs.id}>
                <h2>{blog.title}</h2>
                <p>Written by { blog.author }</p>
+               <button onClick={() => handleDelete(blog.id) }>Delete blog</button>
                </div>
                
                ))} 
--- src/Home.js
+++ src/Home.js
@@ -7,11 +7,17 @@ const Home = () => {
         {title: "Party", body: "loremidf", author:"Ben", id:2},
         {title: "karty", body: "loremidf", author:"Men", id:3},
         {title: "louty", body: "loremidf", author:"Finn", id:4}
-    ])
+    ]);
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter((blog) =>  blog.id !== id);
+        setBlogs(newBlogs)
+    }
+
        
     return ( 
         <div className="home">
-          <Bloglist blogsyasha={blogs} />
+          <Bloglist blogsyasha={blogs}  handleDelete={handleDelete} />
        </div>
      );
 }
